8e37e03415eff68948e93f5ca398d7e8a67d0cd6
fretws <fretws@uw.edu>
2022-02-08 01:14:03 -0800

    Organize the tests differently and update readme

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This code base is for an Android Application called 'Rekognition.' Developed by 
 
 ![architecture diagram](/imgs/Detailed_Architecture.png)
 
-### Sequence diagram showing how an image of text goes from being captured by the user to being read aloud:
+### Sequence diagram showing how an image of text goes from being captured by the user to being read aloud
 
 Using an img tag with a width of 1600px and a height of 1080px did not change the readability of the diagram. Instead, click on the image to get a better viewing experience.
 <!-- <img src="https://user-images.githubusercontent.com/62970170/150383233-6d5f1bfc-9510-489e-bfdf-7942a73f9eaf.png" width="1600" height="1080"> -->
@@ -22,5 +22,6 @@ Using an img tag with a width of 1600px and a height of 1080px did not change th
 
 ### How to run the tests
 
-I have compiled a testing configuration and pushed it to this repo. It relies on Android Studio to be run, and in Android Studio, you can click 'Add Configurations' > 'Gradle' > 'FirebaseFunctionsServiceTestSuite' > 'OK' in order to access this configuration.
-If you do not wish to use android studio, you can run `./gradlew test` from the `rekognition` directory, though this took quite some time to run the first time I ran it, and I do not know how to make it run with coverage.
+Run `./gradlew test` from the `rekognition` directory. The first time I ran it, this took quite some time to run and I had to run it again for any tests to actually execute.
+I also highly recommend using powershell to run this command, because if you use git bash like I usually would, there are odd control characters that make it almost unreadable.
+If this doesn't work or you already have Android Studio, then in Android Studio you can hit 'Ctrl+Shift+F10' or right click on the directory menu and run all tests from the context menu.
--- rekognition/app/src/test/java/edu/uw/minh2804/rekognition/FirebaseFunctionsServiceTestSuite.kt
+++ rekognition/app/src/test/java/edu/uw/minh2804/rekognition/FirebaseFunctionsServiceTestSuite.kt
@@ -13,22 +13,24 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-class FirebaseFunctionsServiceTestSuite {
-    // Text Annotator Test Suite
+
+class TextAnnotatorTestSuite {
     private val textAnnotator = FirebaseFunctionsService.Annotator.TEXT
 
     @Test
-    fun text_onAnnotated_returnsExpectedFormat_normalCase() = assertTextResponse(TestUtils.realTextRecognitionData)
+    fun onAnnotated_returnsExpectedFormat_normalCase() =
+        assertTextResponse(TestUtils.realTextRecognitionData)
 
     @Test
-    fun text_onAnnotated_returnsExpectedFormat_emptyCase() = assertTextResponse("")
+    fun onAnnotated_returnsExpectedFormat_emptyCase() = assertTextResponse("")
 
     @Test
-    fun text_onAnnotated_returnsExpectedFormat_nullCase() = assertTextResponse(null)
+    fun onAnnotated_returnsExpectedFormat_nullCase() = assertTextResponse(null)
 
     private fun assertTextResponse(textResponseToTest: String?) {
         val textAnnotationResponse = TestUtils.TextAnnotationResponseWrapper(textResponseToTest)
-        val actualTextReadyForDisplay = textAnnotator.onAnnotated(textAnnotationResponse.rawTextAnnotatedResponse)
+        val actualTextReadyForDisplay =
+            textAnnotator.onAnnotated(textAnnotationResponse.rawTextAnnotatedResponse)
         val expectedTextReadyForDisplay = textAnnotationResponse.expectedTextReadyForDisplay
         assertEquals(expectedTextReadyForDisplay, actualTextReadyForDisplay)
     }
@@ -36,26 +38,29 @@ class FirebaseFunctionsServiceTestSuite {
     @Test
     // Caution: This test is not sensitive to changes in the toString64 extension of the Bitmap
     // class, whereas the real annotate method is sensitive to these changes
-    fun text_annotate_passesCorrectJson() = assertAnnotatorPassesCorrectJson(textAnnotator, TestUtils.Endpoint.TEXT)
+    fun annotate_passesCorrectJson() =
+        assertAnnotatorPassesCorrectJson(textAnnotator, TestUtils.Endpoint.TEXT)
 
     @Test
-    fun text_formatAnnotationResult_formatsCorrectly() {
-        val actualFormattedResponse = FirebaseFunctionsService.formatAnnotationResult(TestUtils.Endpoint.TEXT.exampleJsonResponse)
+    fun formatAnnotationResult_formatsCorrectly() {
+        val actualFormattedResponse =
+            FirebaseFunctionsService.formatAnnotationResult(TestUtils.Endpoint.TEXT.exampleJsonResponse)
         val expectedFormattedResponse = TestUtils.Endpoint.TEXT.exampleImageAnnotation
         assertEquals(expectedFormattedResponse, actualFormattedResponse)
     }
+}
 
-    // Object Annotator Test Suite
+class ObjectAnnotatorTestSuite {
     private val objectAnnotator = FirebaseFunctionsService.Annotator.OBJECT
 
     @Test
-    fun object_onAnnotated_returnsExpectedFormat_normalCase() = assertObjectResponseFormatsToString(
+    fun onAnnotated_returnsExpectedFormat_normalCase() = assertObjectResponseFormatsToString(
         TestUtils.realObjectRecognitionData,
         TestUtils.realObjectRecognitionData.joinToString{ it.description }
     )
 
     @Test
-    fun object_onAnnotated_returnsExpectedFormat_emptyCase() = assertObjectResponseFormatsToString(listOf(), null)
+    fun onAnnotated_returnsExpectedFormat_emptyCase() = assertObjectResponseFormatsToString(listOf(), null)
 
     private fun assertObjectResponseFormatsToString(objectAnnotations: List<EntityAnnotation>, expectedString: String?) {
         val actualLabelsReadyForDisplay = objectAnnotator.onAnnotated(
@@ -67,64 +72,74 @@ class FirebaseFunctionsServiceTestSuite {
     @Test
     // Caution: This test is not sensitive to changes in the toString64 extension of the Bitmap
     // class, whereas the real annotate method is sensitive to these changes
-    fun object_annotate_passesCorrectJson() = assertAnnotatorPassesCorrectJson(objectAnnotator, TestUtils.Endpoint.OBJECT)
+    fun annotate_passesCorrectJson() = assertAnnotatorPassesCorrectJson(objectAnnotator, TestUtils.Endpoint.OBJECT)
 
     @Test
-    fun object_formatAnnotationResult_formatsCorrectly() {
+    fun formatAnnotationResult_formatsCorrectly() {
         val actualFormattedResponse = FirebaseFunctionsService.formatAnnotationResult(TestUtils.Endpoint.OBJECT.exampleJsonResponse)
         val expectedFormattedResponse = TestUtils.Endpoint.OBJECT.exampleImageAnnotation
         assertEquals(expectedFormattedResponse, actualFormattedResponse)
     }
+}
 
-    private fun assertAnnotatorPassesCorrectJson(annotator: Annotator, endpoint: TestUtils.Endpoint) {
-        mockkStatic(Base64::class)
-        every { Base64.encodeToString(any(), any()) } returns TestUtils.base64EncodedImage
+fun assertAnnotatorPassesCorrectJson(annotator: Annotator, endpoint: TestUtils.Endpoint) {
+    mockkStatic(Base64::class)
+    every { Base64.encodeToString(any(), any()) } returns TestUtils.base64EncodedImage
 
-        val bitmap = mockk<Bitmap>(relaxed = true)
-        every {
-            // toString64 is an extension of the bitmap class and should be tested outside of
-            // this test suite, because it is technically subject to change
-            bitmap.toString64()
-        } returns TestUtils.base64EncodedImage
+    val bitmap = mockk<Bitmap>(relaxed = true)
+    every {
+        // toString64 is an extension of the bitmap class and should be tested outside of
+        // this test suite, because it is technically subject to change
+        bitmap.toString64()
+    } returns TestUtils.base64EncodedImage
 
-        assertEquals(TestUtils.base64EncodedImage, bitmap.toString64())
+    assertEquals(TestUtils.base64EncodedImage, bitmap.toString64())
 
-        mockkObject(Firebase)
+    mockkObject(Firebase)
 
-        mockkObject(FirebaseAuthService)
-        every {
-            FirebaseAuthService.isAuthenticated()
-        } returns true
-        assertTrue(FirebaseAuthService.isAuthenticated())
+    mockkObject(FirebaseAuthService)
+    every {
+        FirebaseAuthService.isAuthenticated()
+    } returns true
+    assertTrue(FirebaseAuthService.isAuthenticated())
 
-        mockkObject(FirebaseFunctionsService)
-        val requestAnnotationArgument = slot<JsonObject>()
+    mockkObject(FirebaseFunctionsService)
+    val requestAnnotationArgument = slot<JsonObject>()
 
-        coEvery {
-            FirebaseFunctionsService.requestAnnotation(
-                requestBody = capture(requestAnnotationArgument)
-            )
-        } answers {
-            println(requestAnnotationArgument.captured)
-            endpoint.exampleImageAnnotation
-        }
-
-        // Assert that annotate Returns the right annotation
-        runBlocking {
-            annotator.annotate(bitmap)
-        }
-
-        // Verify the function is called
-        coVerify {
-            FirebaseFunctionsService.requestAnnotation(any())
-        }
-
-        TestUtils.assertJSONFormatAndContents(
-            json = requestAnnotationArgument.captured,
-            expectedContent = TestUtils.base64EncodedImage,
-            endpoint = endpoint
+    coEvery {
+        FirebaseFunctionsService.requestAnnotation(
+            requestBody = capture(requestAnnotationArgument)
         )
-
-        unmockkAll()
+    } answers {
+        println(requestAnnotationArgument.captured)
+        endpoint.exampleImageAnnotation
     }
+
+    // Assert that annotate Returns the right annotation
+    runBlocking {
+        annotator.annotate(bitmap)
+    }
+
+    // Verify the function is called
+    coVerify {
+        FirebaseFunctionsService.requestAnnotation(any())
+    }
+
+    assertJSONFormatAndContents(
+        json = requestAnnotationArgument.captured,
+        expectedContent = TestUtils.base64EncodedImage,
+        endpoint = endpoint
+    )
+
+    unmockkAll()
+}
+
+fun assertJSONFormatAndContents(json: JsonObject, expectedContent: String, endpoint: TestUtils.Endpoint) {
+    val image:JsonObject = json.getAsJsonObject("image")
+    val actualContent: String = image.getAsJsonPrimitive("content").asString
+    assertEquals(expectedContent, actualContent)
+    val featureArray = json.getAsJsonArray("features")
+    val onlyFeature = featureArray.first().asJsonObject
+    val actualEndpointDescriptor = onlyFeature.getAsJsonPrimitive("type").asString
+    assertEquals(endpoint.firebaseDescriptor, actualEndpointDescriptor)
 }
